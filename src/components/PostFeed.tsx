b574a9e88b30f01b8fa76547b609c84242359994
Michael Mooney <mikeymooney1991@gmail.com>
2023-07-02 11:20:08 -0500

    update PostFeed to show loader on fetching next page

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -9,6 +9,7 @@ import { useSession } from "next-auth/react";
 import { ExtendedPost } from "@/types/db";
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import Post from "./Post";
+import { Loader2 } from "lucide-react";
 
 interface PostFeedProps {
   initialPosts: ExtendedPost[];
@@ -94,6 +95,17 @@ const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
           );
         }
       })}
+
+      {isFetchingNextPage && (
+        <li
+          className="
+            flex
+            justify-center
+          "
+        >
+          <Loader2 className="w-6 h-6 text-zinc-500 animate-spin" />
+        </li>
+      )}
     </ul>
   );
 };
